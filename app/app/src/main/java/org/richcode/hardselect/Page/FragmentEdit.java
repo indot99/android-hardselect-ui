442978a99a18e369940c5a73fc030a5029af8a5f
unknown <popp30@naver.com>
2019-05-02 08:37:35 +0900

    수업가기전에 작업한거 한번 커밋

--- app/app/src/main/java/org/richcode/hardselect/Page/FragmentEdit.java
+++ app/app/src/main/java/org/richcode/hardselect/Page/FragmentEdit.java
@@ -6,10 +6,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.richcode.hardselect.R;
 
 public class FragmentEdit extends Fragment {
+
+    EditText EditQuestion;
+    Button CompleteButton;
+
 
     public FragmentEdit() {
     }
@@ -17,6 +24,18 @@ public class FragmentEdit extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit, container, false);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        EditQuestion = (EditText)view.findViewById(R.id.edit_question);
+        CompleteButton = (Button)view.findViewById(R.id.edit_complete_button);
+
+
+        CompleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
--- app/app/src/main/java/org/richcode/hardselect/Page/FragmentInfo.java
+++ app/app/src/main/java/org/richcode/hardselect/Page/FragmentInfo.java
@@ -3,13 +3,32 @@ package org.richcode.hardselect.Page;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.richcode.hardselect.Adapter.FeedAdapter;
+import org.richcode.hardselect.Data.FeedData;
 import org.richcode.hardselect.R;
 
+import java.util.ArrayList;
+
 public class FragmentInfo extends Fragment {
+
+    Button KakaoButton;
+    Button SelectBoardButton;
+    TextView UserText;
+    TextView HelloUserText;
+
+    RecyclerView MyRecyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
+    ArrayList<FeedData> list = new ArrayList<FeedData>();
 
     public FragmentInfo() {
     }
@@ -17,6 +36,36 @@ public class FragmentInfo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+        UserText = (TextView)view.findViewById(R.id.user_info_text);
+        HelloUserText = (TextView)view.findViewById(R.id.hello_user_text);
+        KakaoButton = (Button)view.findViewById(R.id.kakao_openchatting_button);
+        SelectBoardButton = (Button)view.findViewById(R.id.select_board_button);
+        MyRecyclerView = (RecyclerView)view.findViewById(R.id.list_info);
+
+
+        MyRecyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getContext());
+        MyRecyclerView.setLayoutManager(layoutManager);
+
+        adapter = new FeedAdapter(list,getContext());
+
+        MyRecyclerView.setAdapter(adapter);
+
+        SelectBoardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        KakaoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         return view;
     }
